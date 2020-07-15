f360b7efa41277be3df11b7e962ec1f828dd09a6
agnesaltin <43341692+agnesaltin@users.noreply.github.com>
2020-07-15 08:02:05 +0200

    minor changes

---
+++ src/CharacterCard/charactercard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import "./charactercard.css";
+
+class CharacterCard extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return("");
+    }
+
+}
+
+export default CharacterCard;
--- src/Characters/characters.js
+++ src/Characters/characters.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import {Link} from "react-router-dom";
 import "./characters.css";
+//import firestore from '@react-native-firebase/firestore';
 
 class Characters extends Component {
     constructor(props){
         super(props);
     }
+
+    /*componentDidMount(){
+        
+        firestore().collection("characters").doc("Rpr6n7S6KRgisV5oTkgt").then(function(doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+    }*/
 
     render(){
         return(
